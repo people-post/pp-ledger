83d30c1a45f230a1afabad76bffeb2325307ee2f
Hongwei Yang <yhw1639@gmail.com>
2026-08-27 16:01:17 -0700

    Define Meta only in ledger as an Object typedef shim.
    Drop dependence on common/Meta.h; pin pp-cpp-common to the matching
    commit.

--- src/lib/common/Meta.h
+++ src/lib/common/Meta.h
@@ -1,2 +1,14 @@
 #pragma once
-#include "common/Meta.h"
+
+/**
+ * Ledger-only alias: Meta is Object (human intermediate tree for ltsToMeta).
+ * Implementation lives in pp-cpp-common as Value/Object.
+ */
+#include "common/Value.h"
+
+namespace pp::common {
+
+using Meta = Object;
+using MetaPtr = ObjectPtr;
+
+} // namespace pp::common
